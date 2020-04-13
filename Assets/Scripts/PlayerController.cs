5c050fac0cddaa1a1c75a224ea0ca8f7802cd2b9
StarkTheGnr <lordsomy@hotmail.com>
2020-04-14 01:12:39 +0200

    Created controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	float speed = 5f;
+
+	private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void FixedUpdate()
+	{
+		float haxis = Input.GetAxis("Horizontal");
+		float vaxis = Input.GetAxis("Vertical");
+
+		Vector3 moveVector = new Vector3(haxis, -9.8f, vaxis) * speed * Time.fixedDeltaTime;
+		controller.Move(moveVector);
+	}
+}
